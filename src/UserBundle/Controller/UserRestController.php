ae7bf673178e8cdb4421be11e40966e2fed4d8e0
Valdior <Valdior@DESKTOP-6PR3EIO>
2017-10-20 21:35:46 +0200

    REST user

---
+++ src/UserBundle/Controller/UserRestController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace UserBundle\Controller;
+
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+use FOS\RestBundle\View\View;
+use FOS\RestBundle\Context\Context;
+
+class UserRestController extends Controller
+{
+    public function getUserAction($username){
+        $user = $this->getDoctrine()->getRepository('UserBundle:User')->findOneByUsername($username);
+        if(!is_object($user)){
+          throw $this->createNotFoundException();
+        }
+
+        $view = View::create();
+
+        $view->setData($user);
+        return $view;
+        //return new JsonResponse($user);
+    }
+}
--- src/UserBundle/Entity/User.php
+++ src/UserBundle/Entity/User.php
@@ -4,12 +4,18 @@ namespace UserBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
 use FOS\UserBundle\Model\User as BaseUser;
+use JMS\Serializer\Annotation\ExclusionPolicy;
+use JMS\Serializer\Annotation\Expose;
+use JMS\Serializer\Annotation\Groups;
+use JMS\Serializer\Annotation\VirtualProperty;
 
 /**
  * User
  *
  * @ORM\Table(name="user")
  * @ORM\Entity(repositoryClass="UserBundle\Repository\UserRepository")
+ * 
+ * @ExclusionPolicy("all") 
  */
 class User extends BaseUser
 {
@@ -17,8 +23,21 @@ class User extends BaseUser
      * @ORM\Id
      * @ORM\Column(type="integer")
      * @ORM\GeneratedValue(strategy="AUTO")
+     * @Expose
      */
     protected $id;
+
+    /**
+     * @ORM\Column(type="string", length=150, nullable=true)
+     * @Expose
+     */
+    protected $name;
+
+    /**
+     * @ORM\Column(type="string", length=150, nullable=true)
+     * @Expose
+     */
+    protected $firstname;
 
     public function __construct()
     {
